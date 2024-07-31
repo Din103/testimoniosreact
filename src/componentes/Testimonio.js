873e7ab01726cbf9c21a927fb4ae4ceee23dc098
Din103 <xbeorlegi@gmail.com>
2024-07-31 10:18:30 +0200

    primer commit

---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,21 @@
+//Testimonios.js
+import React from 'react';
+import '../hojas-de-estilo/Testimonio.css';
+
+function Testimonio(props) {
+  return (
+    <div className='contenedor-testimonio'>
+      {/* para añadir imagen no necesita cierre. tiene que ir entre {} y se utiliza la función require */}
+      <img className='imagen-testimonio'
+        src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+        alt='Foto de Emma'/>
+      <div className='contenedor-texto-testimonio'>
+        <p className='nombre-testimonio'> <strong>{props.nombre}</strong>  en {props.pais}</p>
+        <p className='cargo-testimonio'>{props.cargo} en <strong>{props.empresa} </strong> </p>
+        <p className='texto-testimonio'>{props.testimonio}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonio;
